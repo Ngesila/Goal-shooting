a986a297edd363a9245929366ea4c1277b6cf438
Ngesila <>
2023-05-29 13:37:21 +0000

    correct hovering on button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-console.log(connected);
+/**Wait for the DOM to finish loading before starting the game */
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                checkAnswer();
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        });
+    };
