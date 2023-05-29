25657ce7d1aeeef07c8bfd0b3783f7003e8edd8d
Ngesila <>
2023-05-29 18:28:50 +0000

    create functions structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,8 @@
-/**Wait for the DOM to finish loading before starting the game */
-
-document.addEventListener("DOMContentLoaded", function () {
-    let buttons = document.getElementsByTagName("button");
-
-    for (let button of buttons) {
-        button.addEventListener("click", function () {
-            if (this.getAttribute("data-type") === "submit") {
-                checkAnswer();
-            } else {
-                let gameType = this.getAttribute("data-type");
-                runGame(gameType);
-            }
-        });
-    };
+/**create functions structure */
+function rungame() { };
+function kick() { };
+function result() { };
+function morescores() { };
+function lessscores() { };
+displayGoalkeeper(){ };
+displayResults(){ };
