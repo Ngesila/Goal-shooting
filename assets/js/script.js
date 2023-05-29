c5fe35deb35dd0791063b39f963bec336bb00e27
Ngesila <>
2023-05-29 19:39:17 +0000

    add eventlisteners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,30 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let choice = this.getAttribute("data-type");
+                alert(`You clicked ${choice}`);
+            }
+        });
+    }
+});
+
+
+
+
+
 /**create functions structure */
-function rungame() { };
-function kick() { };
-function result() { };
-function morescores() { };
-function lessscores() { };
-displayGoalkeeper(){ };
-displayResults(){ };
+function rungame() { }
+function kick() { }
+function result() { }
+function morescores() { }
+function lessscores() { }
+displayGoalkeeper(){ }
+displayResults(){ }
